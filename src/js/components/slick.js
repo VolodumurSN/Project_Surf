8acc7557c8f91866926bcb7b81eb1ac6fb33f6d3
Vova <vova77gh@gmail.com>
2019-12-30 21:02:53 +0200

    slider arrows

--- src/js/components/slick.js
+++ src/js/components/slick.js
@@ -1,1 +1,6 @@
-/* slider code */
+$('.header-slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<svg class="arrow-icons slider-right"><use xlink:href="#arrow-right"></use></svg>',
+    nextArrow: '<svg class="arrow-icons slider-left"><use xlink:href="#arrow-left"></use></svg>'
+})
