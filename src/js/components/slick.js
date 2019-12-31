845fcef8b501244eaa9ba7b0092d9fb4af335613
Vova <vova77gh@gmail.com>
2019-12-31 22:51:19 +0200

    dots

--- src/js/components/slick.js
+++ src/js/components/slick.js
@@ -1,6 +1,17 @@
 $('.header-slider').slick({
     infinite: true,
     fade: true,
-    prevArrow: '<svg class="arrow-icons slider-right"><use xlink:href="#arrow-right"></use></svg>',
-    nextArrow: '<svg class="arrow-icons slider-left"><use xlink:href="#arrow-left"></use></svg>'
+    nextArrow: '<svg class="arrow-icons slider-right"><use xlink:href="#arrow-right"></use></svg>',
+    prevArrow: '<svg class="arrow-icons slider-left"><use xlink:href="#arrow-left"></use></svg>',
+    asNavFor: '.slider-dots'
 })
+
+$('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header-slider',
+})
+
+$('.slider-dots .slick-slide').on('click', function (event) {
+    $('.header-slider').slick('slickGoTo', $(this).data('slickIndex'));
+ });
